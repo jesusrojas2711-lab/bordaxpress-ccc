2823fc341baab9ece289439af72235f975d9ee02
jesusrojas2711-lab <jesusrojas2711@gmail.com>
2026-05-03 00:24:36 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-document.getElementById("root").innerHTML = `
-  <div style="min-height:100vh;background:#0F1E35;color:white;display:flex;align-items:center;justify-content:center;font-size:40px;font-family:Arial">
-    MAIN JSX FUNCIONANDO
-  </div>
-`;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
