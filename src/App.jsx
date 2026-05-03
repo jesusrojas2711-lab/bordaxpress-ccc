4274127197e0f8ad78ad5614be0733d77e9e7e5f
jesusrojas2711-lab <jesusrojas2711@gmail.com>
2026-05-03 00:06:34 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 export default function App() {
   return (
-    <div style={{
-      minHeight: "100vh",
-      background: "#0F1E35",
-      color: "white",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      fontFamily: "Arial, sans-serif",
-      fontSize: 32
-    }}>
-      BordaXpress CCC funcionando
-    </div>
+    <h1 style={{ color: "red", fontSize: "40px" }}>
+      PRUEBA BORDA XPRESS
+    </h1>
   );
 }
